ccd01a4861c0a9e07e709dcb88585ac20318a197
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-17 16:17:09 +0800

    Changed root to chatrooms#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'chatrooms#index'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+  get 'pages/home'
   get '/users/:user_id/messages' =>'chatrooms#index', as: 'users_messages'
   post '/users/:user_id/messages' =>'messages#create', as: 'create_users_messages'
 end
