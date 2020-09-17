7e854d7bfb94663e718969c871ce96f15b033cf2
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-17 13:12:37 +0800

    Set "pages#home" as root_path/ Added routes for "Index" of chatroom and "Create" of messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  get '/users/:user_id/messages' =>'chatrooms#index', as: 'users_messages'
+  post '/users/:user_id/messages' =>'messages#create', as: 'create_users_messages'
 end
