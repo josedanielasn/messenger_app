86ae41534ae39f8b6fb242d3830ff63f2caa3f00
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-17 12:58:54 +0800

    Created and migrated migration file for message_body with columns: "message_body" , "user_id"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_120039) do
+ActiveRecord::Schema.define(version: 2020_09_16_112744) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "message_body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
