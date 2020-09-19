4f3cb00a367fa71ffa3181423237804ccef6dfb0
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-19 12:14:41 +0800

    On going modification for new feature

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -11,6 +11,8 @@ consumer.subscriptions.create("ChatroomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    $('#message-container').append(data.mod_messages)
+
+    let new_item = $('#message-container').append(data.mod_messages)
+    $("#message-container").attr('id', `current-user-1`);
   }
 });
