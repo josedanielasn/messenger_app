306a414c5819dd0e18c3f23a32327037d8ba8937
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-19 12:15:22 +0800

    Fix failed tests/ Added test to chatroom_channel_contoller_test

--- test/controllers/chatrooms_controller_test.rb
+++ test/controllers/chatrooms_controller_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class ChatroomsControllerTest < ActionDispatch::IntegrationTest
   test "Should get index" do
-    assert_generates "/users/#{users(:newuser_1).id}/messages", { controller: "chatrooms", action: "index", user_id: users(:newuser_1).id}
+    assert_generates "/", { controller: "chatrooms", action: "index"}
   end
 end
