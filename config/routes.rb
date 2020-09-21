2e06956e405d55fc165f0ae349af3a4049d6b63a
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-21 21:01:06 +0800

    Added new routes for creating new channels and for showing existing channels

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  root to: 'chatrooms#index'
+  root to: 'chatrooms#index' 
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'pages/home'
-  get '/users/messages' =>'chatrooms#index', as: 'users_messages'
-  post '/users/messages' =>'messages#create', as: 'create_users_messages'
+  get '/chatroom/:id/general' =>'chatrooms#index', as: 'users_messages'
+  post '/chatroom/:id' =>'messages#create', as: 'create_users_messages'
+  get '/new' =>'chatrooms#new', as: 'new_chatroom'
+  post '/new' =>'chatrooms#create', as: 'create_chatroom'
+  get '/chatroom/:id' => 'chatrooms#show', as: 'chatroom_show'
+
+   # Serve websocket cable requests in-process
+   # Barely understand.
+   mount ActionCable.server => '/cable'
 end
