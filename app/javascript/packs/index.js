7644cdff0081d01dfa3d75c5993920140bf38a1d
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-21 20:52:25 +0800

    No real changes

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,16 @@
+// let indexMessageBody = document.querySelector('.chat-messages')
+
+// function disableScrolling() {
+//   setTimeout(function () {
+//     indexMessageBody.style.overflow = 'hidden';
+//   }, 1000);
+// }
+
+// function enableScrolling() {
+//   indexMessageBody.style.overflow = '';
+// }
+
+// indexMessageBody.addEventListener('onscroll', disableScrolling())
+// indexMessageBody.addEventListener('onmousewheel', enableScrolling())
+// indexMessageBody.addEventListener('onclick', enableScrolling())
+// indexMessageBody.addEventListener('onmousemove', enableScrolling())
