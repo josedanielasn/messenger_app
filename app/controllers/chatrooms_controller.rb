a17d7e9eeaea5aca1c7519e00caf18435f73c3e8
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-23 19:56:56 +0800

    Added new methods: Delete Edit Update

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,15 +1,20 @@
 class ChatroomsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user! 
   def index
-    @current_room = Chatroom.first
+    @general_channel = Chatroom.general_channel[0]
+    if !@general_channel.users.include?(current_user)
+      @general_channel.users << current_user
+    end
+    @current_room =  @general_channel
     @messages = @current_room.messages
-    @new_message = Message.new
+    @new_message = Message.new               
     @chatrooms = current_user.chatrooms
 
   end
 
   def show
     # byebug
+     @general_channel = Chatroom.general_channel[0]
      @current_room = Chatroom.find(params[:id])
      @messages = @current_room.messages
      @new_message = Message.new
@@ -25,10 +30,32 @@ class ChatroomsController < ApplicationController
 
   def create
     @new_chatroom = Chatroom.new(chatrooms_params)
-    @new_chatroom.user_ids << current_user.id
     @new_chatroom.user_ids.map! {|x| x.to_i }
+    @new_chatroom.room_admin = current_user.username
     @new_chatroom.save
-    redirect_to chatroom_show_path(@new_chatroom)
+    @new_chatroom.users << current_user
+    redirect_to show_chatroom_path(@new_chatroom)
+  end
+
+  def edit
+    @chatroom = Chatroom.find(params[:id])
+    @choose_user = User.all.select {|user| user.username != @chatroom.room_admin}
+  end
+
+  def  update
+    @chatroom = Chatroom.find(params[:id])
+
+    if @chatroom.update(params.require(:chatroom).permit(:room_name,:room_admin ,user_ids: []))
+      redirect_to show_chatroom_path(@chatroom)
+    else
+      render 'edit'
+    end
+  end
+
+  def delete 
+    @chatroom = Chatroom.find(params[:id])
+    @chatroom.destroy
+    redirect_to root_path
   end
 
   private
